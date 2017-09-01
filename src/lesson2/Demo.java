3cba92a6b037584dc56cca5d1d8cb990a93a193b
Juli <walarm@meta.ua>
2017-09-01 22:58:34 +0300

    sql lesson3

--- src/lesson2/Demo.java
+++ src/lesson2/Demo.java
@@ -1,5 +1,7 @@
 package lesson2;
 
+import java.util.Date;
+
 /**
  * Created by user on 31.08.2017.
  */
@@ -7,12 +9,14 @@ public class Demo {
     public static void main(String[] args) {
         //java
         //1. create class +
-        //2. create object with some values
+        //2. create object with some values +
         //3. using it
 
+        Order order = new Order(1001,"tool12212", 140, new Date(), new Date());
+
         //db
-        //1. create table with columnes
-        //2. insert some date to the table
+        //1. create table with columnes +
+        //2. insert some date to the table +
         //3.  using it
     }
 }
